970b44ca69f365d00f1b44a400e697dad7165f14
dutofanim <etofanim@gmail.com>
2021-04-14 15:52:14 -0300

    Average Grade refactor.

--- recommendedExercises/averageGrade.c
+++ recommendedExercises/averageGrade.c
@@ -8,14 +8,21 @@ void main() {
   float grade1 = 0, grade2 = 0, grade3 = 0, avgGrade = 0;
 
   printf("Este programa calcula a média de três notas.\n");
-  printf("Por favor digite as notas.\n");
+  
+  printf("Por favor digite a nota 1.\n");
+  scanf("%f", &grade1);
 
-  scanf("%f %f %f", &grade1, &grade2, &grade3);
+  printf("Por favor digite a nota 2.\n");
+  scanf("%f", &grade2);
+
+  printf("Por favor digite a nota 3.\n");
+  scanf("%f", &grade3);
+
   avgGrade = (grade1 + grade2 + grade3) / 3;
 
   if (avgGrade > 7.0) {
-    printf("Aluno aprovado.");
+    printf("A média do aluno foi %0.2f. Aluno aprovado.", avgGrade);
   } else {
-    printf("Aluno reprovado.");
+    printf("A média do aluno foi %0.2f. Aluno reprovado.", avgGrade);
   }
 }
